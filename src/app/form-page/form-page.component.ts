1e129c4afc37b78081f17c633e5a55c799d807f2
Erik Martelli <sircrininer@yahoo.it>
2021-07-11 11:26:49 +0200

    Progresso form

--- src/app/form-page/form-page.component.ts
+++ src/app/form-page/form-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-page',
@@ -10,7 +10,13 @@ export class FormPageComponent implements OnInit {
   form = new FormGroup({
     firstName: new FormControl('', Validators.required),
     lastName: new FormControl('', Validators.required),
+    gender: new FormControl('', Validators.required),
+    birthdate: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    products: new FormArray([]),
   });
+
+  refProducts: FormArray;
 
   quickLinks = [
     {
@@ -18,14 +24,23 @@ export class FormPageComponent implements OnInit {
       destination: 'anagrafica',
     },
     {
-      text: "Qualcos'altro",
-      destination: 'altro',
+      text: 'Prodotti',
+      destination: 'prodotti',
     },
   ];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.refProducts = this.form.controls['products'] as FormArray;
+    this.refProducts.push(
+      new FormGroup({
+        name: new FormControl('', Validators.required),
+        price: new FormControl('', Validators.required),
+        description: new FormControl('', Validators.required),
+      })
+    );
+  }
 
   onSubmit() {}
 }
