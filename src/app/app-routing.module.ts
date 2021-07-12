938fd8c6375ba17cdff5c49342b37f3343caaba7
Erik Martelli <sircrininer@yahoo.it>
2021-07-12 11:51:40 +0200

    Progresso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FormPageComponent } from './form-page/form-page.component';
+import { BookingFormPageComponent } from './booking-form/booking-form-page.component';
 import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ParkDetailsPageComponent } from './park-details/park-details-page.component';
 
+// Naturalmente le route non sarebbero corrette per un vero sito, ma qua sto forzando che la mia "home" sia la landing page, come da flusso.
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'form', component: FormPageComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', component: LandingPageComponent },
+  { path: 'booking/:id', component: BookingFormPageComponent },
+  { path: 'park/:id', component: ParkDetailsPageComponent },
   { path: '**', redirectTo: '' },
 ];
 
